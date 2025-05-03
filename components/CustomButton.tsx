123fc9953a8102712a03bb74f62670bdcdabc958
enesarihan <enessarihan35@icloud.com>
2025-05-04 00:02:27 +0300

    Auth pages has been added.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -45,7 +45,7 @@ const CustomButton = ({
 }: ButtonProps) => (
   <TouchableOpacity
     onPress={onPress}
-    className={`w-full rounded-full flex p-4 flex-row justify-center items-center shadow-md shadow-neutral-400/70  ${getBgVariantStyle(
+    className={`w-full rounded-full flex p-3 flex-row justify-center items-center shadow-md shadow-neutral-400/70  ${getBgVariantStyle(
       bgVariant
     )} ${className}`}
     {...props}
