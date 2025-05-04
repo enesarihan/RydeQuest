7dde86a9112e645f725a14ccf698ba60bc9af5dc
enesarihan <enessarihan35@icloud.com>
2025-05-05 00:46:25 +0300

    feat(api): add user creation endpoint with error handling
    - Implemented a POST endpoint in user+api.ts to insert new users into the database.
    - Added validation for required fields (name, email, clerkId) with appropriate error responses.
    - Introduced a fetch utility in fetch.ts for API calls with loading and error states.
    - Created a custom hook useFetch for easier data fetching in React components.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -28,11 +30,13 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name={"index"} options={{ headerShown: false }} />
-      <Stack.Screen name={"(root)"} options={{ headerShown: false }} />
-      <Stack.Screen name={"(auth)"} options={{ headerShown: false }} />
-      <Stack.Screen name={"+not-found"} />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name={"index"} options={{ headerShown: false }} />
+        <Stack.Screen name={"(root)"} options={{ headerShown: false }} />
+        <Stack.Screen name={"(auth)"} options={{ headerShown: false }} />
+        <Stack.Screen name={"+not-found"} />
+      </Stack>
+    </ClerkProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,20 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Home() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
   return <Redirect href={"/(auth)/welcome"} />;
 }
